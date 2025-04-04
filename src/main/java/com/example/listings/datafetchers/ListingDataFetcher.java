20f638a0c87d8c050bb1d36d12b5c0a4aa9099c9
nestor.huallpa <huallpa.nestor@gmail.com>
2025-04-04 10:33:32 -0300

    Implementin graph ql

---
+++ src/main/java/com/example/listings/datafetchers/ListingDataFetcher.java
@@ -0,0 +1,59 @@
+package com.example.listings.datafetchers;
+
+import com.example.listings.datasources.ListingService;
+import com.example.listings.generated.types.Amenity;
+import com.example.listings.models.ListingModel;
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsData;
+import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.InputArgument;
+import com.netflix.graphql.dgs.DgsDataFetchingEnvironment;
+import org.springframework.beans.factory.annotation.Autowired;
+import graphql.execution.DataFetcherResult;
+import java.util.Map;
+
+
+import java.io.IOException;
+import java.util.List;
+
+@DgsComponent
+public class ListingDataFetcher {
+
+  private final ListingService listingService;
+
+  @Autowired
+  public ListingDataFetcher(ListingService listingService) {
+    this.listingService = listingService;
+  }
+
+  @DgsQuery
+  public DataFetcherResult<List<ListingModel>> getFeaturedListings() throws IOException {
+    List<ListingModel> listings = listingService.featuredListingsRequest();
+    return DataFetcherResult.<List<ListingModel>>newResult()
+            .data(listings)
+            .localContext(Map.of("hasAmenityData", false))
+            .build();
+  }
+
+  @DgsQuery
+  public DataFetcherResult<ListingModel> listing(@InputArgument String id) {
+    ListingModel listing = listingService.listingRequest(id);
+    return DataFetcherResult.<ListingModel>newResult()
+            .data(listing)
+            .localContext(Map.of("hasAmenityData", true))
+            .build();
+  }
+
+  @DgsData(parentType="Listing")
+  public List<Amenity> amenities(DgsDataFetchingEnvironment dfe) throws IOException {
+    ListingModel listing = dfe.getSource();
+    String id = listing.getId();
+
+    Map<String, Boolean> localContext = dfe.getLocalContext();
+    if (localContext != null && localContext.get("hasAmenityData")) {
+      return listing.getAmenities();
+    }
+    return listingService.amenitiesRequest(id);
+  }
+
+}
